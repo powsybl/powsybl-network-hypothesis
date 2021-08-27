bb53e1cf076b7eb4eb9709198fdd2e314f49cd62
RALAMBOTIANA MIORA <miora.ralambotiana@rte-france.com>
2021-08-27 14:51:50 +0200

    First try: create fictitious voltage level to rattach equipment on a line
    Signed-off-by: RALAMBOTIANA MIORA <miora.ralambotiana@rte-france.com>

---
+++ src/main/java/com/powsybl/network/hypothesis/HypothesisUtils.java
@@ -0,0 +1,114 @@
+/**
+ * Copyright (c) 2021, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.powsybl.network.hypothesis;
+
+import com.powsybl.iidm.network.*;
+
+import java.util.function.BiConsumer;
+
+/**
+ * @author Miora Ralambotiana <miora.ralambotiana at rte-france.com>
+ */
+public final class HypothesisUtils {
+
+    public static void createVoltageLevelOnLine(Line line) {
+        createVoltageLevelOnLine(0.5, 0.5, 0.5, 0.5, 0.5, 0.5, line);
+    }
+
+    public static void createVoltageLevelOnLine(double rdp, double xdp, double g1dp, double b1dp, double g2dp, double b2dp,
+                                                Line line) {
+        createVoltageLevelOnLine(rdp, xdp, g1dp, b1dp, g2dp, b2dp, TopologyKind.BUS_BREAKER, line);
+    }
+
+    public static void createVoltageLevelOnLine(double rdp, double xdp, double g1dp, double b1dp, double g2dp, double b2dp,
+                                                TopologyKind topologyKind, Line line) {
+        Network network = line.getNetwork();
+        Substation substation = network.newSubstation()
+                .setId(line.getId() + "_SUBSTATION")
+                .setEnsureIdUnicity(true)
+                .setFictitious(true)
+                .add();
+        VoltageLevel voltageLevel = substation.newVoltageLevel()
+                .setId(line.getId() + "_VL")
+                .setEnsureIdUnicity(true)
+                .setNominalV((line.getTerminal1().getVoltageLevel().getNominalV() + line.getTerminal2().getVoltageLevel().getNominalV()) / 2)
+                .setHighVoltageLimit(Math.max(line.getTerminal1().getVoltageLevel().getHighVoltageLimit(), line.getTerminal2().getVoltageLevel().getHighVoltageLimit()))
+                .setLowVoltageLimit(Math.min(line.getTerminal1().getVoltageLevel().getLowVoltageLimit(), line.getTerminal2().getVoltageLevel().getLowVoltageLimit()))
+                .setTopologyKind(topologyKind)
+                .setFictitious(true)
+                .add();
+        LineAdder adder1 = network.newLine()
+                .setId(line.getId() + "_1")
+                .setEnsureIdUnicity(true)
+                .setVoltageLevel1(line.getTerminal1().getVoltageLevel().getId())
+                .setVoltageLevel2(voltageLevel.getId())
+                .setR(line.getR() * rdp)
+                .setX(line.getX() * xdp)
+                .setG1(line.getG1() * g1dp)
+                .setB1(line.getB1() * b1dp)
+                .setG2(line.getG2() * g2dp)
+                .setB2(line.getB2() * b2dp);
+        LineAdder adder2 = network.newLine()
+                .setId(line.getId() + "_2")
+                .setEnsureIdUnicity(true)
+                .setVoltageLevel1(voltageLevel.getId())
+                .setVoltageLevel2(line.getTerminal2().getVoltageLevel().getId())
+                .setR(line.getR() * (1 - rdp))
+                .setX(line.getX() * (1 - xdp))
+                .setG1(line.getG1() * (1 - g1dp))
+                .setB1(line.getB1() * (1 - b1dp))
+                .setG2(line.getG2() * (1 - g2dp))
+                .setB2(line.getB2() * (1 - b2dp));
+        attachLine(line.getTerminal1(), adder1, (bus, adder) -> adder.setConnectableBus1(bus.getId()), (bus, adder) -> adder.setBus1(bus.getId()), (node, adder) -> adder.setNode1(node));
+        attachLine(line.getTerminal2(), adder2, (bus, adder) -> adder.setConnectableBus2(bus.getId()), (bus, adder) -> adder.setBus2(bus.getId()), (node, adder) -> adder.setNode2(node));
+        if (topologyKind == TopologyKind.BUS_BREAKER) {
+            Bus bus = voltageLevel.getBusBreakerView()
+                    .newBus()
+                    .setId(line.getId() + "_BUS")
+                    .setEnsureIdUnicity(true)
+                    .setFictitious(true)
+                    .add();
+            adder1.setBus2(bus.getId());
+            adder2.setBus1(bus.getId());
+        } else if (topologyKind == TopologyKind.NODE_BREAKER) {
+            voltageLevel.getNodeBreakerView()
+                    .newInternalConnection()
+                    .setNode1(0)
+                    .setNode2(1)
+                    .add();
+            adder1.setNode2(0);
+            adder2.setNode1(1);
+        } else {
+            throw new AssertionError();
+        }
+        adder1.add();
+        adder2.add();
+        line.remove();
+    }
+
+    private static void attachLine(Terminal terminal, LineAdder adder, BiConsumer<Bus, LineAdder> connectableBusSetter,
+                                   BiConsumer<Bus, LineAdder> busSetter, BiConsumer<Integer, LineAdder> nodeSetter) {
+        if (terminal.getVoltageLevel().getTopologyKind() == TopologyKind.BUS_BREAKER) {
+            connectableBusSetter.accept(terminal.getBusBreakerView().getConnectableBus(), adder);
+            Bus bus = terminal.getBusBreakerView().getBus();
+            if (bus != null) {
+                busSetter.accept(bus, adder);
+                adder.setBus1(bus.getId());
+            }
+            terminal.disconnect();
+        } else if (terminal.getVoltageLevel().getTopologyKind() == TopologyKind.NODE_BREAKER) {
+            int node = terminal.getNodeBreakerView().getNode();
+            terminal.disconnect();
+            nodeSetter.accept(node, adder);
+        } else {
+            throw new AssertionError();
+        }
+    }
+
+    private HypothesisUtils() {
+    }
+}
---
+++ src/test/java/com/powsybl/network/hypothesis/HypothesisUtilsTest.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright (c) 2021, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.powsybl.network.hypothesis;
+
+import com.powsybl.commons.AbstractConverterTest;
+import com.powsybl.iidm.network.Network;
+import com.powsybl.iidm.network.test.EurostagTutorialExample1Factory;
+import com.powsybl.iidm.xml.NetworkXml;
+import org.joda.time.DateTime;
+import org.junit.Test;
+
+import java.io.IOException;
+
+/**
+ * @author Miora Ralambotiana <miora.ralambotiana at rte-france.com>
+ */
+public class HypothesisUtilsTest extends AbstractConverterTest {
+
+    @Test
+    public void test() throws IOException {
+        Network network = EurostagTutorialExample1Factory.create();
+        network.setCaseDate(DateTime.parse("2021-08-27T14:44:56.567+02:00"));
+        HypothesisUtils.createVoltageLevelOnLine(network.getLine("NHV1_NHV2_1"));
+        roundTripXmlTest(network, NetworkXml::writeAndValidate, NetworkXml::validateAndRead, "/eurostag-line-split.xml");
+    }
+}
