90a6079695349a3c1797355e7ad409577922a0ee
Marcos de Miguel <demiguelm@aia.es>
2021-11-16 16:58:15 +0100

    increase coverage
    Signed-off-by: Marcos de Miguel <demiguelm@aia.es>

--- src/test/java/com/powsybl/network/hypothesis/HvdcHypothesisTest.java
+++ src/test/java/com/powsybl/network/hypothesis/HvdcHypothesisTest.java
@@ -56,4 +56,14 @@ public class HvdcHypothesisTest extends AbstractConverterTest {
         roundTripXmlTest(network, NetworkXml::writeAndValidate, NetworkXml::validateAndRead, "/foursubstation-nodebreaker.xml");
     }
 
+    @Test
+    public void testReactiveLimitsCurve() throws  IOException {
+        Network network = FourSubstationsNodeBreakerFactory.create();
+        network.setCaseDate(DateTime.parse("2021-11-12T10:53:49.274+01:00"));
+        Generator gen1 = network.getGenerator("GH1");
+        Generator gen2 = network.getGenerator("GH3");
+        gen1.setTargetP(-gen1.getTargetP());
+        HvdcHypothesis.convertGeneratorsToHvdc(gen1, gen2);
+        roundTripXmlTest(network, NetworkXml::writeAndValidate, NetworkXml::validateAndRead, "/foursubstation-curve.xml");
+    }
 }
